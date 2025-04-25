82231b809f8b1a70a3d248729703f523976756b0
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-04-25 11:38:59 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :chats, only: %i[create show] do
+    resources :messages, only: %i[create]
+  end
 end
